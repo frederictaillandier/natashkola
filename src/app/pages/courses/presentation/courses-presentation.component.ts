89dd6d8d23bb2b6f9692d9ec0d4a80dc97bba71a
Fred <maujart@gmail.com>
2020-11-29 12:40:49 +0100

    french page

---
+++ src/app/pages/courses/presentation/courses-presentation.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'pm-courses-presentation',
+  styleUrls: ['./courses-presentation.component.scss'],
+  templateUrl: './courses-presentation.component.html'
+})
+export class CoursesPresentationComponent {
+
+}
--- src/app/pages/landing/landing-register/landing-register.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-  selector: 'pm-landing-register',
-  templateUrl: './landing-register.component.html',
-  styleUrls: ['./landing-register.component.scss']
-})
-export class LandingRegisterComponent {
-}
