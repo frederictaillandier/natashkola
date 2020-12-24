085a0783574e6f0fb6b17cb243243ea24d22e16a
Fred <fred@Talos.mshome.net>
2020-12-24 01:16:25 +0100

    switching send us message button to simply email address

--- src/app/shared/register/register.component.ts
+++ src/app/shared/register/register.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {environment} from '../../../environments/environment';
 
 @Component({
   selector: 'pm-register',
@@ -6,4 +7,7 @@ import {Component} from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+  get Mail(): string {
+    return environment.contact.mail;
+  }
 }
